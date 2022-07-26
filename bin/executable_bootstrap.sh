a2e7be95df7e806d6c5cf0a25f17139dfa367483
Gar Morley <gar.morley@jobvalley.com>
2022-07-26 16:28:29 +0100

    Downloads 1pass directly to avoid homebrew error

--- bin/executable_bootstrap.sh
+++ bin/executable_bootstrap.sh
@@ -16,10 +16,13 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install and setup 1password-cli so chezmoi can retrieve files it needs
-brew install --cask 1password
+curl -o ~/Downloads/1Password.zip https://downloads.1password.com/mac/1Password.zip
+unzip ~/Downloads/1Password.zip -d ~/Downloads/
+open ~/Downloads/1Password\ Installer.app
 brew install --cask 1password/tap/1password-cli
 
-open /System/Volumes/Data/Applications/1Password.app
+read -p "Hit enter when 1Password is fully installed..."
+open /Applications/1Password.app
 read -p "Set up account in 1Password app (and enable biometrics) [enter to continue]"
 
 eval $(op signin --account my.1password.com)
