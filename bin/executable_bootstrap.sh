edc75a8b3ac62cce780d3786ae42fab7fd491f79
Gar Morley <gar.morley@jobvalley.com>
2022-07-26 17:00:23 +0100

    Make bootstrap script more idempotent

--- bin/executable_bootstrap.sh
+++ bin/executable_bootstrap.sh
@@ -8,23 +8,27 @@ sudo -v
 sudo softwareupdate --install --all --agree-to-license --background 
 
 # Install command line tools
-which make || xcode-select --install
+command -v make || xcode-select --install
 
 # Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+if ! command -v brew; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 # Install and setup 1password-cli so chezmoi can retrieve files it needs
-curl -o ~/Downloads/1Password.zip https://downloads.1password.com/mac/1Password.zip
-unzip ~/Downloads/1Password.zip -d ~/Downloads/
-open ~/Downloads/1Password\ Installer.app
-brew install --cask 1password/tap/1password-cli
+if [ ! -d /Applications/1Password.app ]; then
+  curl -o ~/Downloads/1Password.zip https://downloads.1password.com/mac/1Password.zip
+  unzip ~/Downloads/1Password.zip -d ~/Downloads/
+  open ~/Downloads/1Password\ Installer.app
+  read -p "Hit enter when 1Password is fully installed..."
+  open /Applications/1Password.app
+fi
 
-read -p "Hit enter when 1Password is fully installed..."
-open /Applications/1Password.app
 read -p "Set up account in 1Password app (and enable biometrics) [enter to continue]"
 
+command -v op || brew install --cask 1password/tap/1password-cli
 eval $(op signin --account my.1password.com)
 
 # Install and run chezmoi to pull in config and scripts
