8bde2ee56e14f1c30577185cb3c94c9b4dca4cea
Gar Morley <garmorley@gmail.com>
2022-08-03 16:40:34 +0100

    Install oh-my-zsh before applying zshrc from chezmoi

--- bin/executable_bootstrap.sh
+++ bin/executable_bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -veuo pipefail
 
+# Install all oh-my-zsh.
+# Doing this early in the process so we can overwrite it with our custom zshrc, instead
+# of omz overwritting mine.
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 open /System/Volumes/Data/Applications/1Password.app
 read -p "Set up account in 1Password app (and enable biometrics) [enter to continue]"
 
@@ -15,9 +20,6 @@ chezmoi init gar --apply
 
 # Install all applications
 brew bundle
-
-# Install all oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install all languages
 asdf plugin add nodejs
