8983f19d5007243492ca465123d6e12de6ef2395
Gar Morley <garmorley@gmail.com>
2022-08-03 16:30:39 +0100

    Adds oh-my-zsh manually

--- bin/executable_bootstrap.sh
+++ bin/executable_bootstrap.sh
@@ -16,6 +16,9 @@ chezmoi init gar --apply
 # Install all applications
 brew bundle
 
+# Install all oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Install all languages
 asdf plugin add nodejs
 asdf plugin add erlang
